7e4ede7c9aa21efe1c51c427946059a02a4b6174
Sebastian Langhi <sebastian.langhi@skydropx.com>
2023-01-11 13:05:01 -0300

    IAM variables format fix

--- projects/variables.tf
+++ projects/variables.tf
@@ -23,8 +23,8 @@ variable "auto_create_network" {
 variable "billing_account_id" {
   description = "Billing account id."
   type        = string
-  default     = "01E654-CF45ED-8F2561"  
-}
+  default     = "01E654-CF45ED-8F2561"
+  }
 
 variable "ip_ranges" {
   description = "Subnet IP CIDR ranges."
@@ -59,7 +59,7 @@ variable "owners_oshift_nonprod" {
 variable "owners_host_project" {
   description = "Host project for Hub of Hubs owners, in IAM format."
   type        = list(string)
-  default     = ["sebastian@gcpsandbox.cloud"]
+  default     = []
 }
 
 variable "project_services" {
